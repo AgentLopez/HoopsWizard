00fbe337a9318524f845ae4d8c527ae3f4ba7fee
Jorge Cuza <jorge.cuza92@gmail.com>
2021-03-10 11:26:36 -0600

    push current changes

--- getAllPlayers.js
+++ getAllPlayers.js
@@ -2,14 +2,17 @@ const playerSearchTextBox = document.getElementById('playerSearchTextBox')
 const btnPlayers = document.getElementById('btnPlayers')
 const playersUL = document.getElementById('playersUL')
 const playerInfo = document.getElementsByClassName('playerInfo')
+const extraDiv = document.getElementById('extraDiv')
 
 
 btnPlayers.addEventListener('click', function () {
+  extraDiv.innerHTML = ''
   playersUL.innerHTML = ''
   playerSearch()
 })
 
 function playerSearch() {
+  playersUL.innerHTML = ''
   let searchPlayer = playerSearchTextBox.value
   let playersURL = `https://www.balldontlie.io/api/v1/players?search=${searchPlayer}`
   fetch(playersURL)
@@ -23,14 +26,15 @@ function playerSearch() {
                 <li>Team: ${searchPlayer.team.abbreviation}</li>
                 <li>Height: ${searchPlayer.height_feet}'${searchPlayer.height_inches}"</li>
                 <li>Weight: ${searchPlayer.weight_pounds} lbs</li>
-                <button id='playerSeasonStats' onclick='playerStats(${searchPlayer.id})'>get player stats!</button>
+                <button type="button" class="btn btn-outline-success" id='playerSeasonStats' onclick='playerStats(${searchPlayer.id})'>Get Season Stats!</button>
+
                </li>
                <div class='playerInfo'></div>
                `
 
 
       })
-
+      
       playersUL.innerHTML = specificPlayer
 
     })
@@ -41,12 +45,14 @@ function playerSearch() {
 
 
 function playerStats(playerId) {
+  
   let statsURL = `https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${playerId}`
   console.log(statsURL)
   fetch(statsURL)
     .then((response) => {
       return response.json()
     }).then((statistics) => {
+      extraDiv.innerHTML = ''
       console.log(statistics)
       let specificStats = statistics.data.map((seasonsAverages) => {
         return `
@@ -63,7 +69,8 @@ function playerStats(playerId) {
                </h4>`
 
       })
-      playersUL.insertAdjacentHTML('beforeend', specificStats)
+      extraDiv.insertAdjacentHTML('beforeend', specificStats)
+      // playersUL.insertAdjacentHTML('beforeend', specificStats)
 
     })
 }
